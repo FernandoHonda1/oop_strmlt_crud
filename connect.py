05bdd7f79f946ac47508dce8592af43e386fdaf0
FernandoHonda1 <fk.honda@uol.com.br>
2021-04-24 23:20:36 -0300

    first checkpoint

---
+++ connect.py
@@ -0,0 +1,10 @@
+import yaml
+import psycopg2
+
+def connect():
+    
+    with open('babies_params.txt') as params_file:
+    
+        return params_file
+    # connection = psycopg2.connect(**params)
+    # return connection
